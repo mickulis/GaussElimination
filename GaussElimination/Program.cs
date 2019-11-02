abe3bdd6c544533c9658be6612205f9bb36caa6a
mickulis <mkulis@sigma.ug.edu.pl>
2019-11-02 18:39:36 +0100

    gitignore update

---
+++ GaussElimination/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GaussElimination
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Number<double>(2.0);
+            var b = new Number<double>(3.0);
+            Console.WriteLine((a+b).Value);
+        }
+    }
+}
